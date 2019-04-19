4ebd26479dfd6322b5b865e491102329ecc1698e
Yannick Lohse <contact@yannick-lohse.fr>
2019-04-19 15:00:25 +0200

    simple mattermost test

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const express = require('express')
 const helmet = require('helmet')
 
 const app = express()
-
 app.use(helmet())
 
+app.post('/', (req, res) => {
+  res.set('Content-Type', 'application/json')
+  res.json({
+    text: 'yo'
+  })
+})
+
 app.get('*', (req, res) => {
-    res.set('Content-Type', 'text/html')
-    res.status(200).send(`
-        <h1><marquee direction=right>Hello from Express path '/' on Now 2.0!</marquee></h1>
-    `)
+  res.set('Content-Type', 'text/html')
+  res.status(200).send(`
+      <h1>Dep.</h1>
+  `)
 })
 
 module.exports = app
